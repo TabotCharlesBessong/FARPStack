f130cbe3e6bafd20282298fc3e2f992ddb8dd948
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-05-17 07:23:12 +0100

    errors in my codes

---
+++ login_course/backend/app/main.py
@@ -0,0 +1,32 @@
+
+import uvicorn
+from fastapi import FastAPI,APIRouter
+from app.config import db
+
+def init_app():
+  db.init()
+  
+  app = FastAPI(
+    title="Charles App",
+    description="Login Page",
+    version="1"
+  )
+  
+  @app.on_event("startup")
+  async def startup():
+    await db.create_all()
+    
+  @app.on_event("shutdown")
+  async def shutdown():
+    await db.close()
+  
+  return app
+
+app = init_app()
+
+def start():
+  """Launched with 'poetry run start' at root level """
+  uvicorn.run("app.main:app",host="localhost",port="8888",reload=True)
+  
+if __name__ == "__main__":
+  start()
